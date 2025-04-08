4b95d4f513d336f314748e428bdea603cbe4c393
Willians Freitas <williansvpf@gmail.com>
2025-04-08 18:51:42 -0300

    Add arquivos do banco de dados

---
+++ Docs/Contato-sql.sql
@@ -0,0 +1,39 @@
+create database AgendaContato;
+use AgendaContato;
+
+create table Usuario(
+idUsuario int primary key not null auto_increment,
+login varchar(45) not null unique,
+nome varchar(45) not null,
+senha varchar(60) not null,
+salt varchar(29) not null
+);
+
+create table Contato(
+idContato int primary key not null auto_increment,
+nome varchar(45) not null,
+sobrenome varchar(40) not null,
+idUsuario int not null,
+foreign key (idUsuario) references Usuario (idUsuario)
+);
+
+create table TipoContato(
+idTipoContato int primary key not null auto_increment,
+tipo varchar(45) not null unique
+);
+
+create table EnderecoContato(
+idEnderecoContato int primary key not null auto_increment,
+valor varchar(45) not null,
+observacao varchar(45),
+idTipoContato int not null,
+idContato int not null,
+foreign key (idTipoContato) references TipoContato (idTipoContato),
+foreign key (idContato) references Contato (idContato)
+);
+
+insert into TipoContato values(0, "Telefone");
+insert into TipoContato values(0, "Fax");
+insert into TipoContato values(0, "Email");
+insert into TipoContato values(0, "Site");
+
