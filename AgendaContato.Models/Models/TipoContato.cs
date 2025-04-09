22ee4b149929fab069962ecc2781bf5adf5aee72
Willians Freitas <williansvpf@gmail.com>
2025-04-09 11:13:17 -0300

    Adiciona modelos de dados: ContatoModel, EnderecoContatoModel, TipoContatoModel e UsuarioModel

---
+++ AgendaContato.Models/Models/TipoContato.cs
@@ -0,0 +1,9 @@
+namespace AgendaContato.Models.Models
+{
+    public class TipoContatoModel
+    {
+        public int? IdTipoContato { get; set; }
+        public string Tipo { get; set; }
+        public string? Descricao { get; set; }
+    }
+}
---
+++ AgendaContato.Models/Models/UsuarioModel.cs
@@ -0,0 +1,11 @@
+namespace  AgendaContato.Models.Models
+{
+    public class UsuarioModel
+    {
+        public int? IdUsuario { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public string? Salt { get; set; }
+    }
+}
