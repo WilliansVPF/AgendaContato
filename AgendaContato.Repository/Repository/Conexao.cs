ab1a2e46c9a7c176f927c82139e4f26d9fbe2e0b
Willians Freitas <williansvpf@gmail.com>
2025-04-09 15:49:21 -0300

    Corrige nome da variável de ambiente para conexão com o banco de dados

--- AgendaContato.Repository/Repository/Conexao.cs
+++ AgendaContato.Repository/Repository/Conexao.cs
@@ -4,7 +4,7 @@ namespace AgendaContato.Repository.Repository
 {
     public class Conexao
     {
-        private static readonly string _connectionString = Environment.GetEnvironmentVariable("AgendaCOntato");
+        private static readonly string _connectionString = Environment.GetEnvironmentVariable("AgendaContato");
 
         public static MySqlConnection GetConnection() => new MySqlConnection(_connectionString);
     }
