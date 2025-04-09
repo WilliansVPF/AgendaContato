ca937b40ec4ef3fec6a7c1106d41d850d2b0527a
Willians Freitas <williansvpf@gmail.com>
2025-04-09 20:44:06 -0300

    Adiciona método Registrar para cadastro de usuário e validação de email(parcial)

--- AgendaContato.Web/Controllers/HomeController.cs
+++ AgendaContato.Web/Controllers/HomeController.cs
@@ -1,20 +1,49 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using AgendaContato.Models.Models;
+using AgendaContato.Interfaces.Interfaces;
 
 namespace AgendaContato.Web.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly IUsuarioRepository _usuarioRepository;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, IUsuarioRepository usuarioRepository)
     {
+        _usuarioRepository = usuarioRepository;
         _logger = logger;
     }
-
     public IActionResult Index()
     {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Registrar(UsuarioModel usuarioModel)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View("Index");
+        }
+
+        // Verifica se o email já existe
+        if (_usuarioRepository.EmailExiste(usuarioModel.Email))
+        {
+            ModelState.AddModelError("Email", "Email já cadastrado");
+            return View("Index");
+        }
+
+        var usuario = new UsuarioModel
+        {
+            Nome = usuarioModel.Nome,
+            Email = usuarioModel.Email,
+            Senha = usuarioModel.Senha
+        };
+
+
+
         return View();
     }
 
