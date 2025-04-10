3ffd59e0d67a8f6a8ac0f09fecedfbf874833105
Willians Freitas <williansvpf@gmail.com>
2025-04-10 20:40:41 -0300

    Adiciona classe UsuarioLogin com propriedades obrigatórias para email e senha

---
+++ AgendaContato.Models/ViewModels/UsuarioLogin.cs
@@ -0,0 +1,8 @@
+namespace AgendaContato.Models.ViewModels
+{
+    public class UsuarioLogin
+    {
+        public required string Email { get; set; }
+        public required string Senha { get; set; }
+    }
+}
