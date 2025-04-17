6dfe27f5a578a035f53b20b9c31c0c7a662a9b21
Willians Freitas <williansvpf@gmail.com>
2025-04-17 12:14:39 -0300

    refatora a validação de senha para melhorar a legibilidade e a estrutura do código

--- AgendaContato.Application/Validations/ValidaSenha.cs
+++ AgendaContato.Application/Validations/ValidaSenha.cs
@@ -9,11 +9,37 @@ public class ValidaSenha : IValidaSenha
         {
             mensagemErro = string.Empty;
 
+            if (!VerificaSenhaVazial(senha, out mensagemErro)) return false;
+
+            if (!VerificaTamanhoSenha(senha, out mensagemErro)) return false;
+
+            if (!VerificaLetraMaiuscula(senha, out mensagemErro)) return false;
+
+            if (!VerificaLetraMinuscula(senha, out mensagemErro)) return false;
+
+            if (!VerificaLetraNumero(senha, out mensagemErro)) return false;
+
+            if (!VerificaLetraCaracterEspecial(senha, out mensagemErro)) return false;
+            
+            return true;
+        }
+
+        private bool VerificaSenhaVazial(string senha, out string mensagemErro)
+        {
+            mensagemErro = string.Empty;
+
             if (string.IsNullOrWhiteSpace(senha))
             {
                 mensagemErro = "Senha não pode ser vazia.";
                 return false;
             }
+
+            return true;
+        }
+
+        private bool VerificaTamanhoSenha(string senha, out string mensagemErro)
+        {
+            mensagemErro = string.Empty;
 
             if (senha.Length < 8 || senha.Length > 32)
             {
@@ -21,11 +47,25 @@ public class ValidaSenha : IValidaSenha
                 return false;
             }
 
+            return true;
+        }
+
+        private bool VerificaLetraMaiuscula(string senha, out string mensagemErro)
+        {
+            mensagemErro = string.Empty;
+
             if (!Regex.IsMatch(senha, "[A-Z]"))
             {
                 mensagemErro = "A senha deve conter ao menos uma letra maiúscula.";
                 return false;
             }
+
+            return true;
+        }
+
+        private bool VerificaLetraMinuscula(string senha, out string mensagemErro)
+        {
+            mensagemErro = string.Empty;
 
             if (!Regex.IsMatch(senha, "[a-z]"))
             {
@@ -33,11 +73,25 @@ public class ValidaSenha : IValidaSenha
                 return false;
             }
 
+            return true;
+        }
+
+        private bool VerificaLetraNumero(string senha, out string mensagemErro)
+        {
+            mensagemErro = string.Empty;
+
             if (!Regex.IsMatch(senha, "[0-9]"))
             {
                 mensagemErro = "A senha deve conter ao menos um número.";
                 return false;
             }
+
+            return true;
+        }
+
+        private bool VerificaLetraCaracterEspecial(string senha, out string mensagemErro)
+        {
+            mensagemErro = string.Empty;
 
             if (!Regex.IsMatch(senha, "[^a-zA-Z0-9]"))
             {
@@ -47,4 +101,6 @@ public class ValidaSenha : IValidaSenha
 
             return true;
         }
+
+
 }
