0273348e2415440a1fae8731bab53ebc4a07a58c
Willians Freitas <williansvpf@gmail.com>
2025-04-18 21:45:03 -0300

    adiciona o modelo UsuarioSessaoModel para gerenciar informações de sessão do usuário

---
+++ AgendaContato.Models/ViewModels/UsusarioSessaoModel.cs
@@ -0,0 +1,8 @@
+namespace AgendaContato.Models.ViewModels;
+
+public class UsuarioSessaoModel
+{
+    public int? Id { get; set; }
+    public string Nome { get; set; }
+    public string Email { get; set; }
+}
