74f6cc1e94b7357ca62ea1d3323b2f856dc7d106
Willians Freitas <williansvpf@gmail.com>
2025-05-27 17:44:41 -0300

    adiciona modelo EnderecoContatoModel à aplicação

---
+++ AgendaContato.Models/Models/EnderecoContatoModel.cs
@@ -0,0 +1,11 @@
+namespace AgendaContato.Models.Models
+{
+    public class EnderecoContatoModel
+    {
+        public int? IdEnderecoContato { get; set; }
+        public string Valor { get; set; }
+        public string? Observacao { get; set; }
+        public int? IdTipoContato { get; set; }
+        public int? IdContato { get; set; }
+    }
+}
