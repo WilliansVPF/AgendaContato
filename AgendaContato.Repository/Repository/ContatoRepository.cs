cc6f1c59de5e92f1fad59e9bd87a28ec63bef2d7
Willians Freitas <williansvpf@gmail.com>
2025-05-27 20:36:04 -0300

    atualiza método NovoContato para aceitar idUsuario como parâmetro anulável e implementa a inserção de endereço com transação

--- AgendaContato.Repository/Repository/ContatoRepository.cs
+++ AgendaContato.Repository/Repository/ContatoRepository.cs
@@ -6,7 +6,7 @@ namespace AgendaContato.Repository.Repository;
 
 public class ContatoRepository : IContatoRepository
 {
-    public void NovoContato(ContatoModel contato, int idUsuario)   
+    public void NovoContato(ContatoModel contato, int idUsuario)
     {
         string sql = "INSERT INTO Contato VALUES(0, @nome, @sobrenome, @idUsuario)";
 
@@ -31,8 +31,49 @@ public class ContatoRepository : IContatoRepository
         }
     }
 
-    public void NovoContato(ContatoModel contato, EnderecoContatoModel endereco, int idUsuario)
+    public void NovoContato(ContatoModel contato, EnderecoContatoModel endereco, int? idUsuario)
     {
-        throw new NotImplementedException();
+        const string sqlContato = "INSERT INTO Contato VALUES(0, @nome, @sobrenome, @idUsuario); SELECT LAST_INSERT_ID();";
+
+        const string sqlEndereco = "INSERT INTO EnderecoContato VALUES(0, @valor, @observacao, @idTipoContato, @idContato);";
+
+        try
+        {
+            using var connection = Conexao.GetConnection;
+            connection.Open();
+
+            using var transaction = connection.BeginTransaction();
+
+            try
+            {
+                using var cmdContato = new MySqlCommand(sqlContato, connection, transaction);
+                cmdContato.Parameters.AddWithValue("@nome", contato.Nome);
+                cmdContato.Parameters.AddWithValue("@sobrenome", contato.Sobrenome);
+                cmdContato.Parameters.AddWithValue("@idUsuario", idUsuario);
+                int id = Convert.ToInt32(cmdContato.ExecuteScalar());
+
+                using var cmdEndereco = new MySqlCommand(sqlEndereco, connection, transaction);
+                cmdEndereco.Parameters.AddWithValue("@idContato", id);
+                cmdEndereco.Parameters.AddWithValue("@valor", endereco.Valor);
+                cmdEndereco.Parameters.AddWithValue("@observacao", endereco.Observacao);
+                cmdEndereco.Parameters.AddWithValue("@idTipoContato", endereco.IdTipoContato);
+                cmdEndereco.ExecuteNonQuery();
+
+                transaction.Commit();
+            }
+            catch (Exception ex)
+            {
+                transaction.Rollback();
+                throw new Exception($"Erro ao cadastrar o contato '{contato.Nome}': {ex.Message}", ex);
+            }
+        }
+        catch (MySqlException sqlEx)
+        {
+            throw new Exception($"Erro de banco de dados ao cadastrar o contato '{contato.Nome}': {sqlEx.Message}", sqlEx);
+        }
+        catch (Exception ex)
+        {
+            throw new Exception($"Erro inesperado ao cadastrar o contato '{contato.Nome}'.", ex);
+        }
     }
 }
