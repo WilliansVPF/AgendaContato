6f3f28385f9035d4d46e85ed04984e57229bcfa0
Willians Freitas <williansvpf@gmail.com>
2025-06-16 22:40:50 -0300

    remove nullable types de IdContato, Sobrenome e IdUsuario no modelo ContatoModel

--- AgendaContato.Models/Models/ContatoModel.cs
+++ AgendaContato.Models/Models/ContatoModel.cs
@@ -2,9 +2,9 @@ namespace AgendaContato.Models.Models
 {
     public class ContatoModel
     {
-        public int? IdContato { get; set; }
+        public int IdContato { get; set; }
         public string Nome { get; set; }
-        public string? Sobrenome { get; set; }        
-        public int? IdUsuario { get; set; }
+        public string Sobrenome { get; set; }        
+        public int IdUsuario { get; set; }
     }
 }
