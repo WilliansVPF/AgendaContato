6b7cb1a01693f6828aadf87058954f37f7254392
Willians Freitas <williansvpf@gmail.com>
2025-06-16 22:40:58 -0300

    remove nullable type de IdEnderecoContato no modelo EnderecoContatoModel

--- AgendaContato.Models/Models/EnderecoContatoModel.cs
+++ AgendaContato.Models/Models/EnderecoContatoModel.cs
@@ -2,9 +2,9 @@ namespace AgendaContato.Models.Models
 {
     public class EnderecoContatoModel
     {
-        public int? IdEnderecoContato { get; set; }
+        public int IdEnderecoContato { get; set; }
         public string Valor { get; set; }
-        public string Observacao { get; set; }
+        public string? Observacao { get; set; }
         public int IdTipoContato { get; set; }
         public int IdContato { get; set; }
     }
